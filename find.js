131f081153dfb2149ad4e865434745a8a523127a
rtkd <null@no.tld>
2018-02-03 22:01:53 +0100

    Add HTML view.

--- find.js
+++ find.js
@@ -3,6 +3,7 @@ var readline = require('readline');
 var stream = require('stream');
 
 var logInterval = 60000;
+var hostPerFile = 1000;
 
 var cmd = process.argv.slice(2);
 
@@ -10,21 +11,7 @@ var regex = new RegExp(cmd[0], 'i');
 
 var instream = fs.createReadStream(cmd[1]);
 var rl = readline.createInterface(instream);
-
-var i = 0, j = 0, out = [];
-
-var getTime = function(date)
-{
-	var h = date.getHours(), m = date.getMinutes(), s = date.getSeconds();
-
-	return (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s);
-};
-
-var logProgress = function() { console.log('Done: ' + i + ' / Matches: ' + j); };
-
-var status = setInterval(logProgress, logInterval);
-
-console.log('Start: ' + getTime(new Date()));
+var i = 0, j = 0, matches = [];
 
 rl.on('line', function(line)
 {
@@ -36,7 +23,7 @@ rl.on('line', function(line)
 	{
 		item.data = data;
 
-		out.push(JSON.stringify(item));
+		matches.push(item);
 
 		j ++;
 	}
@@ -48,9 +35,40 @@ rl.on('close', function()
 {
 	clearInterval(status);
 
-	fs.writeFile(cmd[2], out.join('\n'));
+	var json = [], html  = ['<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>'];
 
-	fs.appendFile('log/log', cmd[0] + ' // ' + cmd[2] + ' // ' + j + ' Matches\n');
+	matches.forEach(function(item)
+	{
+		json.push(JSON.stringify(item));
+
+		item.data = item.data.match(/<html([\s\S]*?)html>/ig);
+
+		item.data = item.data ? item.data : 'No HTML';
+
+		html.push('<div style="display:inline-block;"><iframe seamless sandbox srcdoc="' + item.data.toString().replace(/"/g, '&quot;') + '"></iframe><div style="margin-bottom:1rem;"><a href="http://' + item.host + '">' + item.host + '</a></div></div>');
+	});
+
+	html.push('</body></html>');
+
+	fs.writeFile(cmd[2], json.join('\n'));
+
+	fs.writeFile(cmd[2] + '.html', html.join('\n\n\n\n\n'));
+
+	fs.appendFile('log/.log', cmd[0] + ' // ' + cmd[1] + ' // ' + cmd[2] + ' // ' + j + ' Hosts\n');
 
 	console.log('End: ' + getTime(new Date()));
 });
+
+var getTime = function(date)
+{
+	var h = date.getHours(), m = date.getMinutes(), s = date.getSeconds();
+
+	return (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s);
+};
+
+var logProgress = function() { console.log('Done: ' + i + ' / Hosts: ' + j); };
+
+var status = setInterval(logProgress, logInterval);
+
+console.log('Start: ' + getTime(new Date()));
+
