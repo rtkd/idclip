00e2c39aefeb380b4f51a2a2de54bc14ef1d2721
rtkd <null@no.tld>
2018-02-10 04:36:09 +0100

    Make HTML build an option.

--- find.js
+++ find.js
@@ -4,6 +4,7 @@ var readline = require('readline');
 var stream = require('stream');
 var jsdom = require('jsdom');
 
+var buildHTML = true;
 var removeScripts = true;
 var replaceImages = true;
 var logInterval = 60000;
@@ -48,103 +49,111 @@ readLine.on('close', function()
 {
 	clearInterval(status);
 
-	var  files = [], json = [], html = [], htmlHead  = '<!DOCTYPE html><html><head><meta charset="utf-8"><style>*{box-sizing:border-box;}nav a{display:inline-block;padding:1rem;}.item{display:inline-block;}.itemURL{margin-bottom:1rem;}</style></head><body>', htmlFoot = '</body></html>';
-
-	var i = 0, j = 0, matchesLength = matches.length;
+	var  json = [];
 
 	matches.forEach(function(item) { json.push(JSON.stringify(item)); });
 
-	while (i < matchesLength)
-	{
-		files[j] = matches.slice(i, i += hostPerFile);
-
-		j ++;
-	}
-
-	files.forEach(function(file, i)
-	{
-		html[i] = [];
-
-		html[i].push(htmlHead);
-
-		var menu = '<nav>';
-
-		for (var j = 0; j < files.length; j ++)
-		{
-			menu += '<a href="' + path.basename(cmd[2], '.json') + '-' + j + '.html' + '">' + j + '</a>';
-		}
-
-		menu += '</nav>';
-
-		html[i].push(menu);
-
-		file.forEach(function(host)
-		{
-			var hostDataHTML = host.data.match(/<html([\s\S]*?)html>/ig);
-
-			hostDataHTML = hostDataHTML ? hostDataHTML.toString() : 'No HTML';
-
-			var { JSDOM } = jsdom;
-
-			var dom = new JSDOM(hostDataHTML);
-
-			if (removeScripts === true)
-			{
-				var headScript = dom.window.document.querySelectorAll('head script');
-				var bodyScript = dom.window.document.querySelectorAll('body script');
-
-				for (var key1 in headScript)
-				{
-					if (headScript.hasOwnProperty(key1))
-					{
-						headScript[key1].remove();
-					}
-				}
-
-				for (var key2 in bodyScript)
-				{
-					if (bodyScript.hasOwnProperty(key2))
-					{
-						bodyScript[key2].remove();
-					}
-				}
-			}
-
-			if (replaceImages === true)
-			{
-				var images = dom.window.document.querySelectorAll('img');
-
-				for (var key3 in images)
-				{
-					if (images.hasOwnProperty(key3))
-					{
-						images[key3].src = 'http://placehold.it/200x100&text=IMAGE REPLACED';
-					}
-				}
-			}
-
-			hostDataHTML = dom.serialize();
-
-			var hostHTML = hostDataHTML ? hostDataHTML.replace(/"/g, '&quot;') : 'No HTML';
-
-			html[i].push('<div class="item"><iframe seamless sandbox srcdoc="' + hostHTML + '"></iframe><div class="itemURL"><a href="http://' + host.host + '">' + host.host + '</a></div></div>');
-		});
-
-		html[i].push(menu);
-
-		html[i].push(htmlFoot);
-
-	});
-
-
 	fs.mkdirSync(path.join(outPath, outFolder));
 
-	html.forEach(function(page, i)
-	{
-		fs.writeFile(path.join(outPath, outFolder, outFolder + '-' + i + '.html'), page.join('\n\n'));
-	});
-
 	fs.writeFile(path.join(outPath, outFolder, outFile), json.join('\n'));
+
+	if (buildHTML === true)
+	{
+		var
+			html = [],
+			files = [],
+			htmlHead  = '<!DOCTYPE html><html><head><meta charset="utf-8"><style>*{box-sizing:border-box;}nav a{display:inline-block;padding:1rem;}.item{display:inline-block;}.itemURL{margin-bottom:1rem;}</style></head><body>',
+			htmlFoot = '</body></html>'
+		;
+
+		var i = 0, j = 0, matchesLength = matches.length;
+
+		while (i < matchesLength)
+		{
+			files[j] = matches.slice(i, i += hostPerFile);
+
+			j ++;
+		}
+
+		files.forEach(function(file, i)
+		{
+			html[i] = [];
+
+			html[i].push(htmlHead);
+
+			var menu = '<nav>';
+
+			for (var j = 0; j < files.length; j ++)
+			{
+				menu += '<a href="' + path.basename(cmd[2], '.json') + '-' + j + '.html' + '">' + j + '</a>';
+			}
+
+			menu += '</nav>';
+
+			html[i].push(menu);
+
+			file.forEach(function(host)
+			{
+				var hostDataHTML = host.data.match(/<html([\s\S]*?)html>/ig);
+
+				hostDataHTML = hostDataHTML ? hostDataHTML.toString() : 'No HTML';
+
+				var { JSDOM } = jsdom;
+
+				var dom = new JSDOM(hostDataHTML);
+
+				if (removeScripts === true)
+				{
+					var headScript = dom.window.document.querySelectorAll('head script');
+					var bodyScript = dom.window.document.querySelectorAll('body script');
+
+					for (var key1 in headScript)
+					{
+						if (headScript.hasOwnProperty(key1))
+						{
+							headScript[key1].remove();
+						}
+					}
+
+					for (var key2 in bodyScript)
+					{
+						if (bodyScript.hasOwnProperty(key2))
+						{
+							bodyScript[key2].remove();
+						}
+					}
+				}
+
+				if (replaceImages === true)
+				{
+					var images = dom.window.document.querySelectorAll('img');
+
+					for (var key3 in images)
+					{
+						if (images.hasOwnProperty(key3))
+						{
+							images[key3].src = 'http://placehold.it/200x100&text=IMAGE REPLACED';
+						}
+					}
+				}
+
+				hostDataHTML = dom.serialize();
+
+				var hostHTML = hostDataHTML ? hostDataHTML.replace(/"/g, '&quot;') : 'No HTML';
+
+				html[i].push('<div class="item"><iframe seamless sandbox srcdoc="' + hostHTML + '"></iframe><div class="itemURL"><a href="http://' + host.host + '">' + host.host + '</a></div></div>');
+			});
+
+			html[i].push(menu);
+
+			html[i].push(htmlFoot);
+		});
+
+		html.forEach(function(page, i)
+		{
+			fs.writeFile(path.join(outPath, outFolder, outFolder + '-' + i + '.html'), page.join('\n\n'));
+		});
+	}
 
 	fs.appendFile('log/.log', cmd[0] + ' // ' + path.join(inPath, inFile) + ' // ' + path.join(outPath, outFolder, outFile) + ' // ' + match + ' Hosts\n');
 
