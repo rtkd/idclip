81d8537466ffc70017f1c1b64d4780c8c28535c6
rtkd <null@no.tld>
2018-02-12 04:46:00 +0100

    Refactor and clean up.

--- find.js
+++ find.js
@@ -1,176 +1,174 @@
-var fs = require('fs');
-var path = require('path');
-var readline = require('readline');
-var stream = require('stream');
-var jsdom = require('jsdom');
-
-var buildHTML = true;
-var removeScripts = true;
-var replaceImages = true;
-var logInterval = 60000;
-var hostPerFile = 500;
-
-var cmd = process.argv.slice(2);
-
-var regex = new RegExp(cmd[0], 'i');
-
-var inPath = path.dirname(cmd[1]);
-var inFile = path.basename(cmd[1]);
-
-var outPath = path.dirname(cmd[2]);
-var outFile = path.basename(cmd[2]);
-var outFileExt = path.extname(cmd[2]);
-var outFolder = path.basename(cmd[2], outFileExt);
-
-var inStream = fs.createReadStream(cmd[1]);
-var readLine = readline.createInterface(inStream);
-
-var current = 0, match = 0, matches = [];
-
-readLine.on('line', function(line)
+(function()
 {
-	var item = JSON.parse(line);
+	"use strict";
 
-	var data = new Buffer(item.data, 'base64').toString('utf8');
+	var fs = require('fs');
+	var path = require('path');
+	var readline = require('readline');
+	var stream = require('stream');
+	var jsdom = require('jsdom');
 
-	if (regex.test(data))
+	var config =
 	{
-		item.data = data;
+		'buildHTML': true,
+		'removeScripts': true,
+		'replaceImages': true,
+		'hostsPerHTMLFile': 5,
+		'logInterval': 60000,
 
-		matches.push(item);
+		'happyKitten': 'http://placehold.it/200x100&text=Image replaced',
+		'logFile': 'log/.log',
+		'htmlHeader': '<!DOCTYPE html><html><head><meta charset="utf-8"><style>*{box-sizing:border-box;}nav a{display:inline-block;padding:1rem;}.item{display:inline-block;}.itemURL{margin-bottom:1rem;}</style></head><body>',
+		'htmlFooter': '</body></html>'
+	};
 
-		match ++;
-	}
+	var log = function(string, isError) { console.log(string); if (isError) process.exit(); };
 
-	current ++;
-});
+	if (!process.argv[2]) log('Argument 1 missing. Expecting Regular Expression.', true);
+	if (!process.argv[3]) log('Argument 2 missing. Expecting source file.', true);
+	if (!process.argv[4]) log('Argument 3 missing. Expecting destination file.', true);
 
-readLine.on('close', function()
-{
-	clearInterval(status);
+	var logCounter = 0;
+	var logProgress = function() { logCounter ++; log('Done: ' + current + ' / Hosts: ' + matches +' / Avg: ' + Math.floor(current / logCounter)); };
+	var logStatus = setInterval(logProgress, config.logInterval);
 
-	var  json = [];
-
-	matches.forEach(function(item) { json.push(JSON.stringify(item)); });
-
-	fs.mkdirSync(path.join(outPath, outFolder));
-
-	fs.writeFile(path.join(outPath, outFolder, outFile), json.join('\n'));
-
-	if (buildHTML === true)
+	var getTime = function(date)
 	{
-		var
-			html = [],
-			files = [],
-			htmlHead  = '<!DOCTYPE html><html><head><meta charset="utf-8"><style>*{box-sizing:border-box;}nav a{display:inline-block;padding:1rem;}.item{display:inline-block;}.itemURL{margin-bottom:1rem;}</style></head><body>',
-			htmlFoot = '</body></html>'
-		;
+		var h = date.getHours(), m = date.getMinutes(), s = date.getSeconds();
+		return (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s);
+	};
 
-		var i = 0, j = 0, matchesLength = matches.length;
+	var splitHostsToFiles = function ()
+	{
+		var files = [];
+		for (var i = 0, ii = 0, iii = hosts.length; i < iii; i += config.hostsPerHTMLFile) { files[ii] = hosts.slice(i, i + config.hostsPerHTMLFile); ii ++; }
+		return files;
+	};
 
-		while (i < matchesLength)
+	var buildMenu = function (pageCount)
+	{
+		var menu = '<nav>';
+		for (var i = 0; i < pageCount; i ++) { menu += '<a href="' + outFileName + '-' + i + '.html' + '">' + i + '</a>'; }
+		return menu += '</nav>';
+	};
+
+	var removeScripts = function (dom) { dom.window.document.querySelectorAll('script').forEach(function(script){ script.remove(); }); };
+
+	var replaceImages = function (dom) { dom.window.document.querySelectorAll('img').forEach(function(image){ image.src = config.happyKitten; }); };
+
+	var isDir = function (path)
+	{
+		try	{ return fs.statSync(path).isDirectory(); }
+		catch (e) { if (e.code === 'ENOENT') return false; else throw e; }
+	};
+
+	var isFile = function (path)
+	{
+		try	{ return fs.statSync(path).isFile(); }
+		catch (e) { if (e.code === 'ENOENT') return false; else throw e; }
+	};
+
+	var regex = new RegExp(process.argv[2], 'i');
+
+	var inDirName = path.dirname(process.argv[3]);
+	var inFileName = path.basename(process.argv[3], path.extname(process.argv[3]));
+
+	var outDirName = path.dirname(process.argv[4]);
+	var outFile = path.basename(process.argv[4]);
+	var outFileName = path.basename(process.argv[4], path.extname(process.argv[4]));
+
+	var inStream = fs.createReadStream(process.argv[3]);
+	var readLine = readline.createInterface(inStream);
+
+	var current = 0, matches = 0, hosts = [];
+
+	readLine.on('line', function(line)
+	{
+		var host = JSON.parse(line);
+		var hostDataDecoded = new Buffer(host.data, 'base64').toString('utf8');
+
+		if (regex.test(hostDataDecoded))
 		{
-			files[j] = matches.slice(i, i += hostPerFile);
-
-			j ++;
+			var item = { 'vhost': host.vhost, 'host': host.host, 'data': hostDataDecoded, 'port': host.port, 'ip': host.ip };
+			hosts.push(item);
+			matches ++;
 		}
 
-		files.forEach(function(file, i)
+		current ++;
+	});
+
+	readLine.on('close', function()
+	{
+		clearInterval(logStatus);
+
+		var  json = [];
+
+		hosts.forEach(function(host) { json.push(JSON.stringify(host)); });
+
+		if (!isDir(path.join(outDirName, inFileName))) fs.mkdirSync(path.join(outDirName, inFileName));
+		if (!isDir(path.join(outDirName, inFileName, outFileName))) fs.mkdirSync(path.join(outDirName, inFileName, outFileName));
+
+		if (!isFile(path.join(outDirName, inFileName, outFileName, outFile))) fs.writeFile(path.join(outDirName, inFileName, outFileName, outFile), json.join('\n'));
+		else
 		{
-			html[i] = [];
+			log('File exists. Renaming..');
+			fs.writeFile(path.join(outDirName, inFileName, outFileName, outFile) + '-' + getTime(new Date()), json.join('\n'));
+		}
 
-			html[i].push(htmlHead);
+		fs.appendFile(config.logFile, process.argv[2] + ' // ' + process.argv[3] + ' // ' + path.join(outDirName, inFileName, outFileName, outFile) + ' // ' + matches + ' Hosts\n');
 
-			var menu = '<nav>';
+		if (config.buildHTML === true)
+		{
+			var htmlFiles = [];
 
-			for (var j = 0; j < files.length; j ++)
+			var hostsPerHTMLFile = splitHostsToFiles();
+
+			var htmlMenu = buildMenu(hostsPerHTMLFile.length);
+
+			hostsPerHTMLFile.forEach(function(file, i)
 			{
-				menu += '<a href="' + path.basename(cmd[2], '.json') + '-' + j + '.html' + '">' + j + '</a>';
-			}
+				htmlFiles[i] = [];
 
-			menu += '</nav>';
+				htmlFiles[i].push(config.htmlHeader);
+				htmlFiles[i].push(htmlMenu);
 
-			html[i].push(menu);
-
-			file.forEach(function(host)
-			{
-				var hostDataHTML = host.data.match(/<html([\s\S]*?)html>/ig);
-
-				hostDataHTML = hostDataHTML ? hostDataHTML.toString() : 'No HTML';
-
-				var { JSDOM } = jsdom;
-
-				var dom = new JSDOM(hostDataHTML);
-
-				if (removeScripts === true)
+				file.forEach(function(host)
 				{
-					var headScript = dom.window.document.querySelectorAll('head script');
-					var bodyScript = dom.window.document.querySelectorAll('body script');
+					var hostDataHTML = host.data.match(/<html([\s\S]*?)html>/ig);
+					hostDataHTML = hostDataHTML ? hostDataHTML.toString() : undefined;
 
-					for (var key1 in headScript)
+					if (hostDataHTML && (config.removeScripts === true || config.replaceImages === true))
 					{
-						if (headScript.hasOwnProperty(key1))
-						{
-							headScript[key1].remove();
-						}
+						var dom = new jsdom.JSDOM(hostDataHTML);
+
+						if (config.removeScripts === true) removeScripts(dom);
+						if (config.replaceImages === true) replaceImages(dom);
+
+						var hostHTML = dom.serialize().replace(/"/g, '&quot;');
+
+						htmlFiles[i].push('<div class="item"><iframe seamless sandbox srcdoc="' + hostHTML + '"></iframe><div class="itemURL"><a href="http://' + host.host + '">' + host.host + '</a></div></div>');
 					}
 
-					for (var key2 in bodyScript)
-					{
-						if (bodyScript.hasOwnProperty(key2))
-						{
-							bodyScript[key2].remove();
-						}
-					}
-				}
+				});
 
-				if (replaceImages === true)
-				{
-					var images = dom.window.document.querySelectorAll('img');
-
-					for (var key3 in images)
-					{
-						if (images.hasOwnProperty(key3))
-						{
-							images[key3].src = 'http://placehold.it/200x100&text=IMAGE REPLACED';
-						}
-					}
-				}
-
-				hostDataHTML = dom.serialize();
-
-				var hostHTML = hostDataHTML ? hostDataHTML.replace(/"/g, '&quot;') : 'No HTML';
-
-				html[i].push('<div class="item"><iframe seamless sandbox srcdoc="' + hostHTML + '"></iframe><div class="itemURL"><a href="http://' + host.host + '">' + host.host + '</a></div></div>');
+				htmlFiles[i].push(htmlMenu);
+				htmlFiles[i].push(config.htmlFooter);
 			});
 
-			html[i].push(menu);
+			htmlFiles.forEach(function(page, i)
+			{
+				if (!isFile(path.join(outDirName, inFileName, outFileName, outFileName + '-' + i + '.html'))) fs.writeFile(path.join(outDirName, inFileName, outFileName, outFileName + '-' + i + '.html'), page.join('\n\n'));
+				else
+				{
+					log('File exists. Renaming..');
+					fs.writeFile(path.join(outDirName, inFileName, outFileName, outFileName + '-' + i + '.html' + '-' + getTime(new Date())), page.join('\n\n'));
+				}
+			});
 
-			html[i].push(htmlFoot);
-		});
+			log('Finish: ' + getTime(new Date()));
+		}
+	});
 
-		html.forEach(function(page, i)
-		{
-			fs.writeFile(path.join(outPath, outFolder, outFolder + '-' + i + '.html'), page.join('\n\n'));
-		});
-	}
-
-	fs.appendFile('log/.log', cmd[0] + ' // ' + path.join(inPath, inFile) + ' // ' + path.join(outPath, outFolder, outFile) + ' // ' + match + ' Hosts\n');
-
-	console.log('End: ' + getTime(new Date()));
-
-});
-
-var getTime = function(date)
-{
-	var h = date.getHours(), m = date.getMinutes(), s = date.getSeconds();
-
-	return (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s);
-};
-
-var logProgress = function() { console.log('Done: ' + current + ' / Hosts: ' + match); };
-
-var status = setInterval(logProgress, logInterval);
-
-console.log('Start: ' + getTime(new Date()));
-
+	log('Start: ' + getTime(new Date()));
+	log('Files will be in: ' + path.join(outDirName, inFileName, outFileName));
+})();
